1e3cc314bec7d6742b68e944f9b7c514e9c64f98
David Kolia <Dkolia@icloud.com>
2018-07-16 12:18:05 -0500

    Added author attributes

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,17 +1,33 @@
 const express = require("express");
 const router = express.Router();
+const Author = require("../models/author");
 
 router.get("/", (req, res) => {
-  res.render("authors/index.ejs");
+  Author.find({}, (err, foundAuthors) => {
+    res.render("authors/index.ejs", {
+      authors: foundAuthors
+    })
+  })
 });
 
 router.get("/new", (req, res) => {
   res.render("authors/new.ejs");
 });
 
+router.get("/:id", (req, res) => {
+  Author.findById(req.params.id, (err, foundAuthor) => {
+    res.render("authors/show.ejs", {
+      author: foundAuthor
+    });
+  });
+});
+
 router.post("/", (req, res) => {
   console.log(req.body);
-  res.send("Server recieved request.")
+  Author.create(req.body, (err, createdAuthor) => {
+    console.log(createdAuthor, " is the createdAuthor");
+    res.redirect("/authors");
+  });
 });
 
 module.exports = router;
